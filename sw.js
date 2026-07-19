5f9aa2c51d2ce2190b5bf9abca8c416ef7ce8bea
Maud3dprint <157945670+Maud3dprint@users.noreply.github.com>
2026-07-19 14:09:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="uke-studio-v1";
+const C="uke-studio-v2";
 self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"])));self.skipWaiting();});
 self.addEventListener("activate",e=>self.clients.claim());
 self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
